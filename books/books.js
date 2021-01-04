ca525e5c1f10300718bdb5b0c3f017c6067d8804
Raffael Martins <devrmartins@gmail.com>
2021-01-04 20:15:19 -0300

    Connect mongodb

--- books/books.js
+++ books/books.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+mongoose.connect("mongodb+srv://admin:45m89zv1.rbm@cluster0.aybul.mongodb.net/booksservice?retryWrites=true&w=majority",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.use(express.json());
 
